e02f271e951e585bae9401a6b159bbb335957afa
hafid27 <hafidrobbi9@gmail.com>
2021-02-28 18:39:09 +0700

    penambahan pengalian

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// perubahan
-// perubahan 1
+		// perubahan 1
+		int hafid = 27;
+		int johan = 12;
+		
+		int total = hafid*johan;
+		System.out.println(total);
 	}
+	
 }
